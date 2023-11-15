ea9a663f0b5f0a3d0026b0ce1ab6a57e1b6d57a0
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-11-15 08:42:13 +0100

    Task 4-6

--- module-1/src/main/java/com/kodilla/multi/deadlock/ProcessB.java
+++ module-1/src/main/java/com/kodilla/multi/deadlock/ProcessB.java
@@ -1,15 +1,19 @@
 package com.kodilla.multi.deadlock;
 
+import static com.kodilla.multi.deadlock.SharedLocks.sharedLock;
+
 public class ProcessB implements Runnable {
    @Override
    public void run() {
       try {
          Thread.sleep(50);
-         synchronized (ResourceB.class) {
-            System.out.println("Process B, part 1");
-            Thread.sleep(50);
-            synchronized (ResourceA.class) {
-               System.out.println("Process B, part 2");
+         synchronized (sharedLock) {
+            synchronized (ResourceB.class) {
+               System.out.println("Process B, part 1");
+               Thread.sleep(50);
+               synchronized (ResourceA.class) {
+                  System.out.println("Process B, part 2");
+               }
             }
          }
       } catch (InterruptedException e) {
